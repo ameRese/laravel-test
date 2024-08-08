e4428abc81eb0c5dfc8d620f63b56cf986e8e65d
ameRese <yarukix@gmail.com>
2024-08-08 20:01:51 +0900

    Vercelデプロイキャンセル

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Http\Request;
-// use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 class TestController extends Controller
 {
     public function test() {
-        // $users = User::all();
-        // return view('test', compact('users'));
-        return view('test');
+        $users = User::all();
+        return view('test', compact('users'));
+        // return view('test');
     }
 }
